12a273ed05d2860aa2c60e346037bf52ee192047
bbo-di <ler01260721@gmail.com>
2026-02-19 18:48:55 +0900

    chore: sync deployment fixes (pg, AI, kakao, analytics)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import type { Metadata } from 'next'
 import { Playfair_Display } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import { AuthProvider } from '@/components/auth-provider'
 import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
@@ -37,7 +36,7 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#F2F4F6" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#0D0D0D" media="(prefers-color-scheme: dark)" />
-        <script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js" integrity="sha384-DKYJZ8NLiK8MN4/C5P2dtSmLQ4KwPaoqAfyA/DfmEc1VDxu4yyC7wy6K1Hs90nk" crossOrigin="anonymous" async></script>
+        <script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js" integrity="sha384-DKYJZ8NLiK8MN4/C5P2dtSmLQ4KwPaoqAfyA/DfmEc1VDxu4yyC7wy6K1Hs90nka" crossOrigin="anonymous" async></script>
       </head>
       <body className="font-sans antialiased" suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
@@ -45,7 +44,6 @@ export default function RootLayout({
             {children}
           </AuthProvider>
         </ThemeProvider>
-        <Analytics />
       </body>
     </html>
   )
