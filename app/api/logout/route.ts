7951f7c55b4733f6e519302b435e6348e23d9c50
bbo-di <ler01260721@gmail.com>
2026-02-23 00:40:48 +0900

    Auto update

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,14 +1,14 @@
 import { logout } from "@/lib/auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const response = NextResponse.redirect("/login");
-
+export async function GET(request: NextRequest) {
   try {
     await logout();
   } catch (error) {
     console.error("Logout error:", error);
   }
+
+  const response = NextResponse.redirect(new URL("/login", request.url));
 
   try {
     response.cookies.delete("weve_session");
